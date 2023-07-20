e0fab968521308b3b76c237461839566226a6751
FatemehAria <ftemewh@gmail.com>
2023-07-20 17:20:30 +0330

    categories & home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../action";
 
 const Home = () => {
   const { data, loading, error } = useSelector((state) => state.products);
-  console.log(data);
+  const [category, setCategory] = useState("");
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProducts());
@@ -12,18 +13,64 @@ const Home = () => {
   return (
     <div>
       <div className="flex justify-center items-center p-5 text-sm gap-4 mx-auto">
-        <p>All</p>
-        <p>Men's Clothing</p>
-        <p>Women's Clothing</p>
-        <p>Jewelery</p>
-        <p className="hidden md:inline-block">Electronic</p>
+        <p onClick={() => setCategory("all")}>All</p>
+        <p onClick={() => setCategory("men's clothing")}>Men's Clothing</p>
+        <p onClick={() => setCategory("women's clothing")}>Women's Clothing</p>
+        <p onClick={() => setCategory("jewelery")}>Jewelery</p>
+        <p
+          className="hidden md:inline-block"
+          onClick={() => setCategory("electronic")}
+        >
+          Electronic
+        </p>
       </div>
+      {data.map((product) => {
+        switch (category) {
+          case product.category:
+            return (
+              <div
+                className="flex justify-center items-center flex-wrap px-10 py-6 m-4 border gap-1 rounded-lg h-72"
+                key={product.id}
+              >
+                <img src={product.image} className="max-w-[60%] max-h-[60%] " />
+                <h4 className="text-center w-full h-10 flex justify-center items-center p-2 my-4">
+                  {product.title}
+                </h4>
+                <h5 className="h-5 p-4 flex justify-center items-center">
+                  ${product.price}
+                </h5>
+              </div>
+            );
 
-      {
-        data.map((product) => <div>{product.title}</div>)
-      }
+          case "all":
+            return (
+              <div
+                className="flex justify-center items-center flex-wrap px-10 py-6 m-4 border gap-1 rounded-lg h-72"
+                key={product.id}
+              >
+                <img src={product.image} className="max-w-[60%] max-h-[60%] " />
+                <h4 className="text-center w-full h-10 flex justify-center items-center p-2 my-4">
+                  {product.title}
+                </h4>
+                <h5 className="h-5 p-4 flex justify-center items-center">
+                  ${product.price}
+                </h5>
+              </div>
+            );
+        }
+      })}
     </div>
   );
 };
 
 export default Home;
+{
+  /* <div
+          className="flex justify-center items-center flex-wrap px-10 py-6 m-4 border gap-1 rounded-lg h-72"
+          key={product.id}
+        >
+          <img src={product.image} className="max-w-[60%] max-h-[60%] " />
+            <h4 className="text-center w-full h-10 flex justify-center items-center p-2 my-4">{product.title}</h4>
+            <h5 className="h-5 p-4 flex justify-center items-center">${product.price}</h5>
+          </div> */
+}
