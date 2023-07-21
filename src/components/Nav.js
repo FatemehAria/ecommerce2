9038ec04043e45ae87248fc60e72fd25141a46b7
FatemehAria <ftemewh@gmail.com>
2023-07-21 15:27:53 +0330

    nav & home responsive,detail started

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,9 +5,39 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   const [show, setShow] = useState(false);
   return (
-    <div>
-      <div className="flex justify-around items-center bg-yellow-900">
+    <div className="lg:h-[40rem] lg:bg-navBg lg:bg-cover lg:bg-center lg:object-cover pt-2 bg-gradient-to-r from-slate-600 to-slate-400">
+      <div className="flex justify-around items-center mb-2 lg:justify-evenly lg:bg-transparent">
         <Link to="/">LA COLLECTION</Link>
+
+        {/* FOR LG */}
+        <div className="hidden lg:inline-block">
+          <ul className="lg:flex lg:gap-20 lg:justify-center lg:items-center">
+            <Link to="/" className="hover:text-gray-600 border-slate-600 hover:border-b-2">Home</Link>
+            <Link to="/products" className="hover:text-gray-600 border-slate-600 hover:border-b-2">Products</Link>
+            <Link to="/aboutus" className="hover:text-gray-600 border-slate-600 hover:border-b-2">About</Link>
+            <Link to="/contactus" className="hover:text-gray-600 border-slate-600 hover:border-b-2">Contact</Link>
+          </ul>
+        </div>
+        <div className="hidden lg:flex lg:gap-5 lg:justify-center lg:items-center">
+          <Link
+            to="/login"
+            className="border border-black py-1 px-4 rounded-lg hover:bg-slate-600 hover:text-white"
+          >
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="border border-black py-1 px-4 rounded-lg hover:bg-slate-600 hover:text-white"
+          >
+            Register
+          </Link>
+          <Link
+            to="/cart"
+            className="border border-black py-1 px-4 rounded-lg hover:bg-slate-600 hover:text-white"
+          >
+            Cart
+          </Link>
+        </div>
         <span className="lg:hidden">
           {!show ? (
             <FaBars onClick={() => setShow(!show)} />
@@ -15,20 +45,9 @@ const Nav = () => {
             <RxCross2 onClick={() => setShow(!show)} />
           )}
         </span>
-
-        {/* FOR LG */}
-        <div className="hidden lg:inline-block">
-          <ul>
-            <Link to="/">Home</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/aboutus">About</Link>
-            <Link to="/contactus">Contact</Link>
-          </ul>
-        </div>
-        {/* ******** */}
       </div>
       {show && (
-        <ul className="flex flex-col items-start justify-center px-16 bg-yellow-600 absolute w-full">
+        <ul className="flex flex-col items-start justify-center px-16 absolute w-full bg-white md:px-[21%] lg:hidden  ">
           <Link to="/" onClick={() => setShow(false)}>
             Home
           </Link>
@@ -44,15 +63,15 @@ const Nav = () => {
         </ul>
       )}
 
-      <div className="flex justify-evenly items-center bg-yellow-700 lg:inline-block">
+      <div className="flex justify-evenly items-center lg:hidden">
         <Link to="/login">Login</Link>
         <Link to="/register">Register</Link>
         <Link to="/cart">Cart</Link>
       </div>
       {/* Pic */}
-      <div>
-        <img src={require("../imgs/Shopping7.png")} alt="" />
-      </div>
+      {/* <div className="lg:hidden">
+        <img src={require('../imgs/Shopping7.png')} alt="shopping"/>
+      </div> */}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        navBg: "url('/src/imgs/Shopping7Cropped.png')",
+      },
+    },
   },
   plugins: [],
-}
+};
